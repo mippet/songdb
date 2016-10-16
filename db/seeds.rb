52e0b841cbcd33cbec3f09d005655db39d560000
mippet <pimluitjes@gmail.com>
2016-10-16 12:50:24 +0200

    add models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Song.delete_all
+Artist.delete_all
+
+stevie_wonder = Artist.create(name: " Stevie Wonder")
+typhoon = Artist.create(name: " Typhoon")
+doe_maar = Artist.create(name: " Doe Maar")
+
+song1 = Song.create(name: "AS", artist: stevie_wonder)
+song2 = Song.create(name: "Superstition", artist: stevie_wonder)
+song3 = Song.create(name: "Zandloper", artist: typhoon)
+song4 = Song.create(name: "Niet weglopen", artist: typhoon)
+song5 = Song.create(name: "De bom", artist: doe_maar)
+song6 = Song.create(name: "Pa", artist: doe_maar)
